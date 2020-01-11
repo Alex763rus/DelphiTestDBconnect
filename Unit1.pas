6102c4c28ccada0d18e1c1faeac94599a6832f92
alex163rus <163lexa16394@mail.ru>
2020-01-11 13:08:18 +0800

    Устойчивое состояние, правильный игнор

--- Unit1.pas
+++ Unit1.pas
@@ -4,10 +4,18 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Data.DB, Data.Win.ADODB,
+  Vcl.Grids, Vcl.DBGrids, Vcl.ExtCtrls;
 
 type
   TForm1 = class(TForm)
+    ADOConnection1: TADOConnection;
+    Panel1: TPanel;
+    Memo1: TMemo;
+    Button1: TButton;
+    DBGrid1: TDBGrid;
+    ADOQuery1: TADOQuery;
+    DataSource1: TDataSource;
     procedure Button1Click(Sender: TObject);
     procedure RadioButton1Click(Sender: TObject);
   private
@@ -25,7 +33,11 @@ implementation
 
 procedure TForm1.Button1Click(Sender: TObject);
 begin
-//real
+//ShowMessage('hello!');
+ADOQuery1.SQL.Clear;
+ADOQuery1.SQL.Add('Select * from tbl1');
+ADOQuery1.Open;
+
 end;
 
 procedure TForm1.RadioButton1Click(Sender: TObject);
